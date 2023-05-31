c79c4b83f8a4b767a4142ea8cfb73d8b405b0a47
Axiaa <91245292+Axiaaa@users.noreply.github.com>
2023-05-31 21:24:58 +0200

    Update presence.py

--- commandes/presence.py
+++ commandes/presence.py
@@ -1,28 +1,31 @@
 from interactions import *
 from random import randint
+import asyncio
 
 
 class Presence(Extension): 
-  @listen()
-  async def on_startup():
-    while True:
-          random_activity = randint(1, 2)
-          if random_activity == 1:
-              await bot.change_presence(
-                  activity=inter.Activity(
-                      name="games",
-                      type=inter.ActivityType.PLAYING,
-                  )
-              )
-          elif random_activity == 2:
-              await bot.change_presence(
-                  activity=inter.Activity(
-                      name="a movie",
-                      type=inter.ActivityType.WATCHING,
-                  )
-              )
-          await asyncio.sleep(60)
+
+    @listen()
+    async def on_startup(self):
+        while True:
+                random_activity = randint(1, 2)
+                if random_activity == 1:
+                    await self.bot.change_presence(
+                        activity=Activity(
+                            name="games",
+                            type=ActivityType.PLAYING,
+                        )
+                    )
+                elif random_activity == 2:
+                    await self.change_presence(
+                        activity=Activity(
+                            name="a movie",
+                            type=ActivityType.WATCHING,
+                        )
+                    )
+                await asyncio.sleep(60)
+            
           
-          
- def setup(bot):
-  Presence(bot)
+def setup(bot):
+    Presence(bot)
+
